b9e18cfef916bade80ccaaaea0fb11c24c512637
theshka <t.heshka@gmail.com>
2015-01-15 16:22:23 -0500

    Fixed indentation

--- includes/class.visitorTracking.php
+++ includes/class.visitorTracking.php
@@ -1,13 +1,17 @@
 <?php
-class visitorTracking {
-
+class visitorTracking 
+{
+	//Stores "thisVisit" array
 	var $thisVisit = null;
+	
+	//MYSQLi connection 
 	private $link = null;
 	
 	/**
 	 * CLASS CONSTRUCTOR 
 	 */
-	public function __construct() {
+	public function __construct() 
+	{
 		
 		//Initialize the database 
 		$this->db_connect();
@@ -20,30 +24,35 @@ class visitorTracking {
 	/**
 	 * CLASS DESTRUCTOR 
 	 */
-	public function __destruct(){
-	
-		//Disconnect the database
-		if( $this->link){
+	public function __destruct()
+	{
 		
+		//Disconnect the database
+		if( $this->link )
+		{
 			$this->link->close();
-			
 		}
-	
+		
 	}
     
 	/**
 	 * Connect to the database
 	 */
-	private function db_connect() {
+	private function db_connect() 
+	{
 		
 		//Establish MYSQLi link 
 		mb_internal_encoding( 'UTF-8' );
 		mb_regex_encoding( 'UTF-8' );
 		mysqli_report( MYSQLI_REPORT_STRICT );
-		try {
+	
+		try 
+		{
 			$this->link = new mysqli( DB_HOST, DB_USER, DB_PASS, DB_NAME );
 			$this->link->set_charset( "utf8" );
-		} catch ( Exception $e ) {
+		} 
+		catch ( Exception $e ) 
+		{
 			die( 'Unable to connect to database' );
 		}
 		
@@ -52,93 +61,97 @@ class visitorTracking {
 	/**
 	 * Track visit, insert in database
 	 */
-	private function track(){
+	private function track()
+	{
 	
 		//Prepare variables
 		$visitor_ip 		= GetHostByAddr($this->getIP());
-		$ip_location 		= $this->geoCheckIP($this->getIP());
-		$visitor_city 		= $ip_location['town'];
-		$visitor_state 		= $ip_location['state'];
-		$visitor_country 	= $ip_location['country'];
+		$ip_location		= $this->geoCheckIP($this->getIP());
+		$visitor_city		= $ip_location['town'];
+		$visitor_state		= $ip_location['state'];
+		$visitor_country	= $ip_location['country'];
 		$visitor_flag		= $this->getFlag($this->getIP());
-		$visitor_browser 	= $this->getBrowserType();
-		$visitor_OS			= $this->getOS();
-		$visitor_date 		= $this->getDate("Y-m-d h:i:sA");
-		$visitor_day 		= $this->getDate("d");
-		$visitor_month 		= $this->getDate("m");
-		$visitor_year 		= $this->getDate("Y");
-		$visitor_hour 		= $this->getDate("h");
-		$visitor_minute 	= $this->getDate("i");
-		$visitor_seconds 	= $this->getDate("s");
-		$visitor_referer 	= $this->getReferer();
-		$visitor_page 		= $this->getRequestURI();
+		$visitor_browser	= $this->getBrowserType();
+		$visitor_OS		= $this->getOS();
+		$visitor_date		= $this->getDate("Y-m-d h:i:sA");
+		$visitor_day		= $this->getDate("d");
+		$visitor_month		= $this->getDate("m");
+		$visitor_year		= $this->getDate("Y");
+		$visitor_hour		= $this->getDate("h");
+		$visitor_minute		= $this->getDate("i");
+		$visitor_seconds	= $this->getDate("s");
+		$visitor_referer	= $this->getReferer();
+		$visitor_page		= $this->getRequestURI();
 		
 		//Gather variables in array
 		$visitor = array(
-			'visitor_ip' => $visitor_ip,
-			'visitor_city' => $visitor_city,
-			'visitor_state' => $visitor_state,
-			'visitor_country' => $visitor_country,
-			'visitor_flag' => $visitor_flag,
-			'visitor_browser' => $visitor_browser,
-			'visitor_OS' => $visitor_OS,
-			'visitor_date' => $visitor_date,
-			'visitor_day' => $visitor_day,
-			'visitor_month' => $visitor_month,
-			'visitor_year' => $visitor_year,
-			'visitor_hour' => $visitor_hour,
-			'visitor_minute' => $visitor_minute,
-			'visitor_seconds' => $visitor_seconds,
-			'visitor_referer' => $visitor_referer,
-			'visitor_page' => $visitor_page
+			'visitor_ip' 		=> $visitor_ip,
+			'visitor_city' 		=> $visitor_city,
+			'visitor_state' 	=> $visitor_state,
+			'visitor_country' 	=> $visitor_country,
+			'visitor_flag' 		=> $visitor_flag,
+			'visitor_browser' 	=> $visitor_browser,
+			'visitor_OS' 		=> $visitor_OS,
+			'visitor_date' 		=> $visitor_date,
+			'visitor_day' 		=> $visitor_day,
+			'visitor_month' 	=> $visitor_month,
+			'visitor_year' 		=> $visitor_year,
+			'visitor_hour' 		=> $visitor_hour,
+			'visitor_minute' 	=> $visitor_minute,
+			'visitor_seconds' 	=> $visitor_seconds,
+			'visitor_referer' 	=> $visitor_referer,
+			'visitor_page' 		=> $visitor_page
 		);
 		
 		//Make sure the array isn't empty
-        if( empty( $visitor ) )
-        {
-            return false;
-        }
+        	if( empty( $visitor ) )
+        	{
+            		return false;
+        	}
         
-        //Insert the data
-        $sql = "INSERT INTO `visitors`";
-        $fields = array();
-        $values = array();
-        foreach( $visitor as $field => $value )
-        {
-            $fields[] = $field;
-            $values[] = "'".$value."'";
-        }
-        $fields = ' (' . implode(', ', $fields) . ')';
-        $values = '('. implode(', ', $values) .')';
-        
-        $sql .= $fields .' VALUES '. $values;
-
-        $query = $this->link->query( $sql );
-        
-        if( $this->link->error )
-        {
-            //return false; 
-            die ( 'ERROR! Please check your database settings.' );
-            return false;
-        }
-        else
-        {
-        	//set thisVisit variable equal to visitor array
-        	$this->thisVisit = $visitor;
+        	//Insert the data
+        	$sql = "INSERT INTO `visitors`";
+        	$fields = array();
+        	$values = array();
         	
-        	//return true
-            return true;
-        }
+        	foreach( $visitor as $field => $value )
+        	{
+            		$fields[] = $field;
+            		$values[] = "'".$value."'";
+        	}
+        	$fields = ' (' . implode(', ', $fields) . ')';
+        	$values = '('. implode(', ', $values) .')';
+        
+        	$sql .= $fields .' VALUES '. $values;
+
+        	$query = $this->link->query( $sql );
+        
+        	if( $this->link->error )
+        	{
+        	   //return false; 
+        	   die ( 'ERROR! Please check your database settings.' );
+        	   return false;
+        	}
+        	else
+        	{
+        		//set thisVisit variable equal to visitor array
+        		$this->thisVisit = $visitor;
+        	
+        		//return true
+        		return true;
+            
+        	}
 		
 	}
 	
 	/**
 	 * Get visitor IP address
 	 */	
-	private function getIP() {
+	private function getIP() 
+	{
 		
-		if ( ! empty( $_SERVER['REMOTE_ADDR'] ) ){
-	 
+		if ( ! empty( $_SERVER['REMOTE_ADDR'] ) )
+		{
 			$ip = $_SERVER['REMOTE_ADDR'];
 			
 			return $ip;
@@ -151,57 +164,63 @@ class visitorTracking {
 	/**
 	 * Geo-locate visitor IP address
 	 */
-	private function geoCheckIP($ip) {
-	
-		   //check, if the provided ip is valid
-		   if(!filter_var($ip, FILTER_VALIDATE_IP) || $ip == 'localhost')
-		   {
-				   //throw new InvalidArgumentException("IP is not valid");
-				   return false;
-		   }
-	
-		   //contact ip-server
-		   $response=@file_get_contents('http://www.netip.de/search?query='.$ip);
-		   if (empty($response))
-		   {
-				   //throw new InvalidArgumentException("Error contacting Geo-IP-Server");
-				   return false;
-		   }
-	
-		   //Array containing all regex-patterns necessary to extract ip-geoinfo from page
-		   $patterns=array();
-		   $patterns["domain"] = '#Domain: (.*?)&nbsp;#i';
-		   $patterns["country"] = '#Country: (.*?)&nbsp;#i';
-		   $patterns["state"] = '#State/Region: (.*?)<br#i';
-		   $patterns["town"] = '#City: (.*?)<br#i';
-	
-		   //Array where results will be stored
-		   $ipInfo=array();
-	
-		   //check response from ipserver for above patterns
-		   foreach ($patterns as $key => $pattern)
-		   {
-				   //store the result in array
-				   $ipInfo[$key] = preg_match($pattern,$response,$value) && !empty($value[1]) ? $value[1] : 'not found';
-		   }
-	
+	private function geoCheckIP($ip) 
+	{
+		
+		//check, if the provided ip is valid
+		if(!filter_var($ip, FILTER_VALIDATE_IP) || $ip == 'localhost')
+		{
+			//throw new InvalidArgumentException("IP is not valid");
+			return false;
+		}
+		   
+		//contact ip-server
+		$response=@file_get_contents('http://www.netip.de/search?query='.$ip);
+		if (empty($response))
+		{
+			//throw new InvalidArgumentException("Error contacting Geo-IP-Server");
+			return false;
+		}
+		   
+		//Array containing all regex-patterns necessary to extract ip-geoinfo from page
+		$patterns=array();
+		$patterns["domain"] = '#Domain: (.*?)&nbsp;#i';
+		$patterns["country"] = '#Country: (.*?)&nbsp;#i';
+		$patterns["state"] = '#State/Region: (.*?)<br#i';
+		$patterns["town"] = '#City: (.*?)<br#i';
+		   
+ 		//Array where results will be stored
+		$ipInfo=array();
+		   
+		//check response from ipserver for above patterns
+		foreach ($patterns as $key => $pattern)
+		{
+			//store the result in array
+		$ipInfo[$key] = preg_match($pattern,$response,$value) && !empty($value[1]) ? $value[1] : 'not found';
+		}
+		   
 		   return $ipInfo;
+		   
 	}
 	
 	/**
 	 * Get country flag 
 	 */
-	private function getFlag($ip) {
-
+	private function getFlag($ip) 
+	{
+		
 		$flag = '<img src="http://api.hostip.info/flag.php?ip=' . $ip . '" heigh="30px" width="30px"/>';
 		
 		return $flag;
+		
 	}
 	
 	/**
 	 * Get the visitor browser-type
 	 */
-	private function getBrowserType () {	
+	private function getBrowserType () 
+	{
+		
 		if (!empty($_SERVER['HTTP_USER_AGENT'])) 
 		{ 
 		   $HTTP_USER_AGENT = $_SERVER['HTTP_USER_AGENT']; 
@@ -251,80 +270,80 @@ class visitorTracking {
 		}
 		
 		return $browser_agent;
+		
 	}
 	
 	/**
 	 * Get the visitor operating system
 	 */
-	private function getOS() { 
-
-		$user_agent		=	$_SERVER['HTTP_USER_AGENT'];
-	
-		$os_platform    =   "Unknown OS Platform";
-	
-		$os_array       =   array(
-								'/windows nt 6.3/i'     =>  'Windows 8.1',
-								'/windows nt 6.2/i'     =>  'Windows 8',
-								'/windows nt 6.1/i'     =>  'Windows 7',
-								'/windows nt 6.0/i'     =>  'Windows Vista',
-								'/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
-								'/windows nt 5.1/i'     =>  'Windows XP',
-								'/windows xp/i'         =>  'Windows XP',
-								'/windows nt 5.0/i'     =>  'Windows 2000',
-								'/windows me/i'         =>  'Windows ME',
-								'/win98/i'              =>  'Windows 98',
-								'/win95/i'              =>  'Windows 95',
-								'/win16/i'              =>  'Windows 3.11',
-								'/macintosh|mac os x/i' =>  'Mac OS X',
-								'/mac_powerpc/i'        =>  'Mac OS 9',
-								'/linux/i'              =>  'Linux',
-								'/ubuntu/i'             =>  'Ubuntu',
-								'/iphone/i'             =>  'iPhone',
-								'/ipod/i'               =>  'iPod',
-								'/ipad/i'               =>  'iPad',
-								'/android/i'            =>  'Android',
-								'/blackberry/i'         =>  'BlackBerry',
-								'/webos/i'              =>  'Mobile'
-							);
-	
-		foreach ($os_array as $regex => $value) { 
-	
-			if (preg_match($regex, $user_agent)) {
+	private function getOS() 
+	{ 
+		
+		$user_agent	=	$_SERVER['HTTP_USER_AGENT'];
+		$os_platform	=	"Unknown OS Platform";
+		$os_array	=	array(
+						'/windows nt 6.3/i'     =>  'Windows 8.1',
+						'/windows nt 6.2/i'     =>  'Windows 8',
+						'/windows nt 6.1/i'     =>  'Windows 7',
+						'/windows nt 6.0/i'     =>  'Windows Vista',
+						'/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+						'/windows nt 5.1/i'     =>  'Windows XP',
+						'/windows xp/i'         =>  'Windows XP',
+						'/windows nt 5.0/i'     =>  'Windows 2000',
+						'/windows me/i'         =>  'Windows ME',
+						'/win98/i'              =>  'Windows 98',
+						'/win95/i'              =>  'Windows 95',
+						'/win16/i'              =>  'Windows 3.11',
+						'/macintosh|mac os x/i' =>  'Mac OS X',
+						'/mac_powerpc/i'        =>  'Mac OS 9',
+						'/linux/i'              =>  'Linux',
+						'/ubuntu/i'             =>  'Ubuntu',
+						'/iphone/i'             =>  'iPhone',
+						'/ipod/i'               =>  'iPod',
+						'/ipad/i'               =>  'iPad',
+						'/android/i'            =>  'Android',
+						'/blackberry/i'         =>  'BlackBerry',
+						'/webos/i'              =>  'Mobile'
+					);
+		
+		foreach ($os_array as $regex => $value) 
+		{ 
+			if (preg_match($regex, $user_agent)) 
+			{
 				$os_platform    =   $value;
 			}
-	
 		}   
-	
+		
 		return $os_platform;
-	
+		
 	}
 	
 	/**
 	 * Get the date bits, used for search/filtering
 	 */
-	private function getDate($i) {
-
+	private function getDate($i) 
+	{
+		
 		//get the requested date
 		$date = date($i);
 		
 		//return the date
 		return $date;
-
+		
 	}
 	
 	/**
 	 * Get the referring page, if any is sent
 	 */
-	private function getReferer() {
- 
-		$ref = false;
-	 
-		if ( ! empty( $_SERVER['HTTP_REFERER'] ) ){
-	 
+	private function getReferer() 
+	{
+		
+		if ( ! empty( $_SERVER['HTTP_REFERER'] ) )
+		{
 			$ref = $_SERVER['HTTP_REFERER'];
 			
 			return $ref;
-	 	}
+		}
 		
 		return false;
 	 
@@ -337,8 +356,8 @@ class visitorTracking {
 		
 		$uri = false;
 		
-		if ( ! empty( $_SERVER['REQUEST_URI'] ) ){
-	 
+		if ( ! empty( $_SERVER['REQUEST_URI'] ) )
+		{
 			$uri = $_SERVER['REQUEST_URI'];
 			
 			return $uri;
@@ -351,14 +370,18 @@ class visitorTracking {
 	/**
 	 * Display the current visit array
 	 */	
-	public function displayThisVisit() {
+	public function displayThisVisit() 
+	{
+		
 		print_r($this->thisVisit);
+		
 	}
 	
 	/**
 	 * Display the visitors table
 	 */
-	public function displayVisitors() {           
+	public function displayVisitors() 
+	{           
 		
 		/**
 		 * Retrieving a single row of data
@@ -375,21 +398,26 @@ class visitorTracking {
 			$totalpages = ceil($numrows / $rowsperpage);
 			
 			// get the current page or set a default
-			if (isset($_GET['paginate']) && is_numeric($_GET['paginate'])) {
+			if (isset($_GET['paginate']) && is_numeric($_GET['paginate'])) 
+			{
 			   // cast var as int
 			   $paginate = (int) $_GET['paginate'];
-			} else {
+			} 
+			else 
+			{
 			   // default page num
 			   $paginate = 1;
-			} // end if
+			} 
 			
 			// if current page is greater than total pages...
-			if ($paginate > $totalpages) {
+			if ($paginate > $totalpages) 
+			{
 			   // set current page to last page
 			   $paginate = $totalpages;
 			} // end if
 			// if current page is less than first page...
-			if ($paginate < 1) {
+			if ($paginate < 1) 
+			{
 			   // set current page to first page
 			   $paginate = 1;
 			} // end if
@@ -398,25 +426,25 @@ class visitorTracking {
 			$offset = ($paginate - 1) * $rowsperpage;
 			
 		}
-
+		
 		echo '
 		<table id="visitor" class="table">
-		<thead>				
-			<th>IP Address</th>
-			<th>Browser</th>
-			<th>OS</th>
-			<th>City</th>
-			<th>State</th>
-			<th>Country</th>
-			<th>Date</th>
-			<th>Referer</th>
-			<th>Page</th>
-		</thead>
-		<tbody>
+			<thead>				
+				<th>IP Address</th>
+				<th>Browser</th>
+				<th>OS</th>
+				<th>City</th>
+				<th>State</th>
+				<th>Country</th>
+				<th>Date</th>
+				<th>Referer</th>
+				<th>Page</th>
+			</thead>
+			<tbody>
 		';
 		
 		$results = $this->link->query( "SELECT * FROM `visitors` ORDER BY `id` DESC LIMIT $offset, $rowsperpage" );
-
+		
 		if( $this->link->error )
 		{
 			return false;
@@ -455,7 +483,8 @@ class visitorTracking {
 		$range = 3;
 		
 		// if not on page 1, don't show back links
-		if ($paginate > 1) {
+		if ($paginate > 1) 
+		{
 		   // show << link to go back to page 1
 		   echo "<a href='{$_SERVER['PHP_SELF']}?paginate=1'> First </a>";
 		   // get previous page num
@@ -465,29 +494,33 @@ class visitorTracking {
 		} // end if 
 		
 		// loop to show links to range of pages around current page
-		for ($x = ($paginate - $range); $x < (($paginate + $range) + 1); $x++) {
-		   // if it's a valid page number...
-		   if (($x > 0) && ($x <= $totalpages)) {
-			  // if we're on current page...
-			  if ($x == $paginate) {
-				 // 'highlight' it but don't make a link
-				 echo "<a>$x</a>";
-			  // if not current page...
-			  } else {
+		for ($x = ($paginate - $range); $x < (($paginate + $range) + 1); $x++) 
+		{
+			// if it's a valid page number...
+			if (($x > 0) && ($x <= $totalpages)) 
+			{
+				// if we're on current page...
+				if ($x == $paginate) {
+					// 'highlight' it but don't make a link
+					echo "<a>$x</a>";
+				// if not current page...
+				} 
+				else {
 				 // make it a link
 				 echo "<a href='{$_SERVER['PHP_SELF']}?&paginate=$x'>$x</a>";
-			  } // end else
-		   } // end if 
+				} // end else
+			} // end if 
 		} // end for
 						 
 		// if not on last page, show forward and last page links        
-		if ($paginate != $totalpages) {
-		   // get next page
-		   $nextpage = $paginate + 1;
+		if ($paginate != $totalpages) 
+		{
+			// get next page
+			$nextpage = $paginate + 1;
 			// echo forward link for next page 
-		   echo "<a href='{$_SERVER['PHP_SELF']}?paginate=$nextpage'> > </a>";
-		   // echo forward link for lastpage
-		   echo "<a href='{$_SERVER['PHP_SELF']}?paginate=$totalpages'> Last </a>";
+			echo "<a href='{$_SERVER['PHP_SELF']}?paginate=$nextpage'> > </a>";
+			// echo forward link for lastpage
+			echo "<a href='{$_SERVER['PHP_SELF']}?paginate=$totalpages'> Last </a>";
 		} // end if
 		/****** end build pagination links ******/
 		
