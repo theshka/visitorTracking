487c10322c8de02a9367dfdc8b6134b5009ea8ae
theshka <t.heshka@gmail.com>
2015-01-15 00:35:06 -0500

    Create db.php
    Database definition file.

---
+++ includes/db.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * define the database
+ */
+define( 'DB_HOST', 'localhost' ); // set database host
+define( 'DB_USER', 'root' ); // set database user
+define( 'DB_PASS', '' ); // set database password
+define( 'DB_NAME', 'skeleton' ); // set database name
